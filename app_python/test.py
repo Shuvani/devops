8dde50fb496c69b4322b51699b2d16605b50f543
Shuvani <gnomik2311@mail.ru>
2021-08-19 22:43:00 +0300

    change version

--- app_python/test.py
+++ app_python/test.py
@@ -8,5 +8,5 @@ def test__get__moscow__time__returns__string():
 
 
 def test__index__get_request__returns_200():
-    request, response = app.test_client.get("/v2.0.0/")
+    request, response = app.test_client.get("/v2.0.2/")
     assert response.status == 200
