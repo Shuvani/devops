8d64fe04476f46e6faa152b1e1cebc95ad8a69a5
Shuvani <gnomik2311@mail.ru>
2021-09-01 12:20:48 +0300

    Change versions

--- app_python/setup.py
+++ app_python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="devops",
-    version="3.3.2",
+    version="3.4.1",
     author='shuva',
     author_email='a.gorb@innopolis.university',
     description='This app shows the current Moscow time'
