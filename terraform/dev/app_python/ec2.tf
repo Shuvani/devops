f0e3edde8ec69eda062c58c812a66ab94334514d
Shuvani <gnomik2311@mail.ru>
2021-09-06 01:57:51 +0300

    Add ansible docker

--- terraform/dev/app_python/ec2.tf
+++ terraform/dev/app_python/ec2.tf
@@ -15,8 +15,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-03d5c68bab01f3496"
   instance_type = "t2.micro"
+  key_name      = "DevopsKey"
+  security_groups = ["DevopsSecurityGroup"]
 
   tags = {
     Name = "Moscow_time"
