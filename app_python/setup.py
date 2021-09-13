98c3bd8ab9311247c55e12e74b863dfa66013c85
Shuvani <gnomik2311@mail.ru>
2021-09-13 09:53:25 +0300

    finish logs with promtail, loki and graphana

--- app_python/setup.py
+++ app_python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="devops",
-    version="6.0.3",
+    version="7.0.0",
     author='shuva',
     author_email='a.gorb@innopolis.university',
     description='This app shows the current Moscow time'
