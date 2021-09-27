d8c7b7900ad8ee6cd8a01fda8a16eac436478213
Shuvani <gnomik2311@mail.ru>
2021-09-27 21:27:37 +0300

    Add persistency to my app

--- app_python/setup.py
+++ app_python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="devops",
-    version="12.3.0",
+    version="12.4.0",
     author='shuva',
     author_email='a.gorb@innopolis.university',
     description='This app shows the current Moscow time'
